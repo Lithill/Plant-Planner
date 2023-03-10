7d589f1c914b3cd8ee6c129f2c07a36c7e197463
Rossanne <rossanneh@gmail.com>
2023-03-10 11:02:16 +0000

    Add images and styling

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="My Plants")
 
 
 @app.route("/login")
