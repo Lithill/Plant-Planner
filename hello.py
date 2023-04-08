25903046d50f902f52ffb51a4047e9c3ab3f0865
Rossanne <rossanneh@gmail.com>
2023-04-08 17:18:48 +0000

    Add Names file

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+# Change this key when put it in gitignore
+app.config['SECRET_KEY'] = "g_EQCyO0}um0MFmst0.:0cwUb0T1[f9f"
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 # Create a route decorator
@@ -34,3 +44,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template(
+        "name.html",
+        name=name,
+        form=form)
