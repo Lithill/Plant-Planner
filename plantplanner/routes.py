e446da757fdc5d43703b116fc02176dc48d24977
Rossanne <rossanneh@gmail.com>
2023-04-10 19:42:11 +0000

    Add delete post functionality

--- plantplanner/routes.py
+++ plantplanner/routes.py
@@ -240,3 +240,24 @@ def edit_post(id):
     form.slug.data = post.slug
     form.content.data = post.content
     return render_template('edit_post.html', form=form)
+
+
+@app.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post_to_delete = Posts.query.get_or_404(id)
+
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        # Return a message
+        flash("Blog post was deleted")
+        # Grab all the posts from the database
+        posts = Posts.query.order_by(Posts.date_posted)
+        return render_template("posts.html", posts=posts)
+
+    except:
+        # Return an error message
+        flash("There was a problem deleting the post")
+        # Grab all the posts from the database
+        posts = Posts.query.order_by(Posts.date_posted)
+        return render_template("posts.html", posts=posts)
