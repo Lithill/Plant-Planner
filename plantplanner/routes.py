fd49f7c61dcde777cb1d6125453e556fc849928d
Rossanne <rossanneh@gmail.com>
2023-04-11 15:53:54 +0000

    Add update profile function to dashboard

--- plantplanner/routes.py
+++ plantplanner/routes.py
@@ -134,6 +134,7 @@ def update(id):
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
         name_to_update.favourite_colour = request.form['favourite_colour']
+        name_to_update.username = request.form['username']
         try:
             db.session.commit()
             flash("User Updated Successfully!")
@@ -301,6 +302,35 @@ def logout():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+    form = UserForm()
+    id = current_user.id
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favourite_colour = request.form['favourite_colour']
+        name_to_update.username = request.form['username']
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template(
+                "dashboard.html",
+                form=form,
+                name_to_update=name_to_update,
+                id=id)
+        except:
+            flash("It looks like something went wrong... Please try again")
+            return render_template(
+                "dashboard.html",
+                form=form,
+                name_to_update=name_to_update,
+                id=id)
+    else:
+        return render_template(
+            "dashboard.html",
+            form=form,
+            name_to_update=name_to_update,
+            id=id)
     return render_template('dashboard.html')
 
 
