6ae4f319418b6282460f444013b15099a4a429fd
Rossanne <rossanneh@gmail.com>
2023-04-11 16:27:24 +0000

    Separate forms and models

--- plantplanner/models.py
+++ plantplanner/models.py
@@ -1,14 +1,10 @@
 from plantplanner import db
 from datetime import datetime
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms import ValidationError
-from wtforms.validators import DataRequired, EqualTo, Length
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_wtf import FlaskForm
-from wtforms.widgets import TextArea
 from flask_login import UserMixin, login_user, LoginManager
 from flask_login import login_required, logout_user, current_user
+from plantplanner.webforms import LoginForm, PostForm, UserForm, PasswordForm
+from plantplanner.webforms import NamerForm
 
 
 # Create Users Model
@@ -46,53 +42,3 @@ class Posts(db.Model):
     author = db.Column(db.String(255))
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
     slug = db.Column(db.String(255))
-
-
-# Create Post Form
-class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    content = StringField("Content", validators=[DataRequired()], widget=TextArea())
-    author = StringField("Author", validators=[DataRequired()])
-    slug = StringField("Slug", validators=[DataRequired()])
-    submit = SubmitField("Submit")
-
-
-# Create a UserForm Class
-class UserForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    username = StringField("Username", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired()])
-    favourite_colour = StringField("Favourite Colour")
-    password_hash = PasswordField(
-        'Password',
-        validators=[DataRequired(), EqualTo(
-            'password_hash2',
-            message='Passwords must match!'
-            )]
-        )
-    password_hash2 = PasswordField(
-        'Confirm password',
-        validators=[DataRequired()]
-        )
-    submit = SubmitField("Submit")
-
-
-# Create a Form Class
-class NamerForm(FlaskForm):
-    name = StringField("What's Your Name", validators=[DataRequired()])
-    submit = SubmitField("Submit")
-
-
-class PasswordForm(FlaskForm):
-    email = StringField("What's your email?", validators=[DataRequired()])
-    password_hash = PasswordField(
-        "What's your password?", validators=[DataRequired()]
-        )
-    submit = SubmitField("Submit")
-
-
-# Create login form
-class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Submit")
---
+++ plantplanner/webforms.py
@@ -0,0 +1,56 @@
+from plantplanner import db
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms import ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.widgets import TextArea
+
+
+# Create Post Form
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()], widget=TextArea())
+    author = StringField("Author", validators=[DataRequired()])
+    slug = StringField("Slug", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+# Create a UserForm Class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    favourite_colour = StringField("Favourite Colour")
+    password_hash = PasswordField(
+        'Password',
+        validators=[DataRequired(), EqualTo(
+            'password_hash2',
+            message='Passwords must match!'
+            )]
+        )
+    password_hash2 = PasswordField(
+        'Confirm password',
+        validators=[DataRequired()]
+        )
+    submit = SubmitField("Submit")
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class PasswordForm(FlaskForm):
+    email = StringField("What's your email?", validators=[DataRequired()])
+    password_hash = PasswordField(
+        "What's your password?", validators=[DataRequired()]
+        )
+    submit = SubmitField("Submit")
+
+
+# Create login form
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
