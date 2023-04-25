c0d17dc6fd1ca0715f2535caa333e38a4a3b17ff
Rossanne <rossanneh@gmail.com>
2023-04-25 21:08:17 +0000

    Start deployment setup

--- plantplanner/__init__.py
+++ plantplanner/__init__.py
@@ -10,7 +10,14 @@ app = Flask(__name__)
 # Add CKEditor
 ckeditor = CKEditor(app)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 UPLOAD_FOLDER = 'static/images/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
